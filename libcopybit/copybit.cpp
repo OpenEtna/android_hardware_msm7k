f29c1370001a239fb08ee97dadab25c0e2af37ff
polytheus <polytheus@googlemail.com>
2010-10-01 11:32:25 +0200

    copybit: Use our header, do not error on wrong dimensions
    Change-Id: Ieb185b4676f2cc98b72cc40bb3489c706f0a4ab4

--- libcopybit/copybit.cpp
+++ libcopybit/copybit.cpp
@@ -200,6 +200,7 @@ static void set_infos(struct copybit_context_t *dev, struct mdp_blit_req *req) {
     req->alpha = dev->mAlpha;
     req->transp_mask = MDP_TRANSP_NOP;
     req->flags = dev->mFlags /*| MDP_BLEND_FG_PREMULT*/;
+    req->sharpening_strength = 64;
 }
 
 /** copy the bits */
@@ -368,7 +369,7 @@ static int stretch_copybit(
         if (src_rect->l < 0 || src_rect->r > src->w ||
             src_rect->t < 0 || src_rect->b > src->h) {
             // this is always invalid
-            return -EINVAL;
+            return 0; /* do not spoil the log */
         }
 
         if (src->w > MAX_DIMENSION || src->h > MAX_DIMENSION)
